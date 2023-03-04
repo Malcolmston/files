068d171ec5aed46db34fe0b616ef51134f51d306
Malcolmston <67117350+Malcolmston@users.noreply.github.com>
2023-03-03 20:57:33 -0500

    major
    adds guest_account, basic_account, and admin_account as well as ejs

--- index.js
+++ index.js
@@ -102,46 +102,322 @@ Admin_Account, File} = require("./sql.js");
 
 
 var express = require('express');
+var session = require("express-session");
 var formidable = require('formidable');
 var path = require('path');
 var fs = require('fs');
 var url = require("url")
 
 const file = new File()
+const guest_account = new Guest_Account()
+const basic_account = new Basic_Account()
+const admin_account = new Admin_Account()
 
 
+var ejs = require('ejs')
 
 
 var app = express();
 
-app.get('/', function (req, res){
-    res.sendFile(__dirname + '/index.html');
+
+app.use(express.static('public'))
+
+const sessionMiddleware = session({
+	secret: 'mysecret',
+	resave: true,
+	saveUninitialized: true
 });
 
-app.post("/get", async (req, res) => {
-  const queryObject = url.parse(req.url, true).query;
 
 
-//  let all = await Files.findAll()
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(sessionMiddleware);
+
+app.get('/', (req, res) => {
+	res.status(200).render('login', {
+		code: 'sign up',
+		fail_code: req.session.username ? 'you are alredy logged in' : ''
+
+	});
+});
+
+app.post('/signup', (req, res) => {
+	let { username, password } = req.body
+
+	basic_account.validate(username, password).then(function(x){
+		// if there is an account with the username and password it is valid.
+		if( !x ){
+			basic_account.create(username, password).then(function(e){
+				req.session.username = username
+        req.session.type = 'basic'
+
+				res.status(200).redirect('home')
+			})
+
+
+		}else{
+      res.redirect('/')
+			res.status(403).render('login', {
+			code: 'an account with that username already exists!',
+			fail_code: 'an account with that username already exists!'
+		});
+		}
+	})
+})
+
+app.post('/login', (req, res) => {
+	let { username, password } = req.body
+  basic_account.isDeleted(username).then(function(bool){
+    if( bool ){
+      res.redirect('/')
+      res.status(403).render('login', {
+code: 'an account with that username or password no longer exists!',
+fail_code: 'The account you are trying to find an account that has been deleted!'
+});
+    }else{
+      basic_account.validate(username, password).then(function(x){
+        if( x ){
+          req.session.username = username
+
+          req.session.type = 'basic'
+
+          res.status(200).redirect('home')
+        }else{
+            admin_account.validate(username, password).then(function(x){
+            if( x ){
+                 req.session.username = username
+              req.session.type = 'admin'
+
+              res.status(200).redirect('home')
+            }else{
+              res.redirect('/')
+              res.status(402).render('login', {
+      code: 'an account with that username or password does not exist!',
+      fail_code: 'an account with that username or password does not exist!'
+    });
+            }
+
+      })
+      }
+    })
+    }
+  })
+
+})
+
+
+app.post('/guest', (req, res) => {
+  guest_account.create().then(function(x){
+    req.session.username = 'guest'
+    req.session.type = 'guest'
+
+    res.status(200).redirect('home')
+  }).catch( e => {
+    console.log( e )
+      res.redirect('/')
+      res.status(500).render('login', {
+			code: 'You are unable to create a guest account!',
+			fail_code: 'Mhmmmmmm You are unable to create a guest account!',
+		});
+  })
+})
+
+
+
+app.get('/home', (req, res) => {
+  if (!req.session.username) {
+console.log('Please enter a name')
+
+res.redirect('/')
+
+/*
+res.status(400).render('login', {
+  code: 'sign up',
+  fail_code: 'you tried to go to a page that you do not have permision to acses'
+
+});*/
+
+
+  }else{
+
+	if (req.session.username) {
+		res.render('home',{
+			type: req.session.type
+		})
+
+	} else {
+    res.redirect('/')
+
+		res.status(400).render('login', {
+			code: 'sign up',
+			fail_code: 'you tried to go to a page that you do not have permision to acses'
+
+		});
+  
+	}
+}
+})
+
+
+app.get('/logout', (req, res) => {
+  if( req.session.type == 'guest' ){
+    guest_account.removeNow().then(function(){
+      req.session.destroy()
+      res.redirect('/')
+      //return;
+    })
+  }else{
+	req.session.destroy()
+  res.redirect('/')
+}
+})
+
+
+
+app.get('/basic/allData', async (req, res) => {
+  res.set('Content-Type', 'application/json')
+
+  let data = await  admin_account.getAll('basic')
+
+  res.send(data )
+})
+
+app.post('/basic/usernameChange', async (req, res) => {
+  let { password, N_username} = req.body
+let b = await basic_account.validate( req.session.username , password)
+//let a = await admin_account.validate(username, password)
+
+if( b){
+basic_account.update_username(req.session.username , password, N_username).then(
+  res.status(200).render('home',{
+    type: req.session.type
+  })
+)
+}else{
+  res.status(403)
+}
+
+})
+
+app.post('/basic/passwordChange', async (req, res) => {
+  let { password, N_password} = req.body
+let b = await basic_account.validate( req.session.username , password)
+//let a = await admin_account.validate(username, password)
+
+if( b){
+basic_account.update_password(req.session.username , password, N_password).then(
+  res.status(200).render('home',{
+    type: req.session.type
+  })
+)
+}else{
+  res.status(403)
+}
+
+})
+
+app.post('/basic/softRemove', async (req, res) => {
+  let { password} = req.body
+
+    basic_account.remove(req.session.username, password).then(function(x){
+      if( x){
+        req.session.destroy()
+        res.redirect('/')
+        res.status(200).render('login', {
+          code: 'sign up',
+          fail_code: 'your account was deleted'
+
+        });
+
+      }else{
+        res.status(403)
+      }
+    })
+})
+
+
+
+app.get('/admin/allData', async (req, res) => {
+  res.set('Content-Type', 'application/json')
+
+  let data = await  admin_account.getAll('admin')
+
+  res.send(data )
+})
+
+app.post('/admin/softRemove', async (req, res) => {
+  let { password, p_username, p_password} = req.body
+
+
+  admin_account.soft_remove(p_username, p_password, req.session.username , password).then(function(x){
+    if(x){
+      res.status(200).render('home',{
+        type: req.session.type
+      })
+    }else{
+      res.status(403)
+    }
+  })
+
+})
+
+app.post('/admin/restore', async (req, res) => {
+  let { password, p_username, p_password} = req.body
+
+
+  admin_account.restore(p_username, p_password, req.session.username , password).then(function(x){
+    if(x){
+      res.status(200).render('home',{
+        type: req.session.type
+      })
+    }else{
+      res.status(403)
+    }
+  })
+
+})
+
+app.post('/admin/hardRemove', async (req, res) => {
+  let { password, p_username, p_password} = req.body
+
+
+  admin_account.hard_remove(p_username, p_password, req.session.username , password).then(function(x){
+    if(x){
+      res.status(200).render('home',{
+        type: req.session.type
+      })
+    }else{
+      res.status(403)
+    }
+  })
+
+})
+
+
+
+
+app.get("/home/get", async (req, res) => {
+
+ let all = await file.findYours(req.session.username)
 
   res.json({
-    all: []//all,
+    all: all,
 
   });
 
 });
 
-app.get("/open", async (req, res) => {
+app.get("/home/open", async (req, res) => {
   const queryObject = url.parse(req.url, true).query;
 
-
-  //let name = req.body.name;
-
-  let all = await Files.findOne({
-    where:{ 
-      name: queryObject.name
-    }
-  });
+  let all = await file.find(queryObject.name)
 
   res.setHeader('Content-Type', all.type)
   res.end(all.rawData)
@@ -149,7 +425,7 @@ app.get("/open", async (req, res) => {
 });
 
 
-app.post('/',  function (req, res){
+app.post('/', function (req, res){
     var form = new formidable.IncomingForm();
 
     form.parse(req);
@@ -166,7 +442,7 @@ app.post('/',  function (req, res){
 			var rawData = fs.readFileSync(oldPath)
 
 
-              let x = await file.create(false,{ 
+              let x = await file.create(req.session.username,{ 
                 name:  (files.name || files.originalFilename), 
                 type: files.mimetype,
                 url: icon(files.mimetype.split('/')[1].trim()),
