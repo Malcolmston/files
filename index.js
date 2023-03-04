6941a0f2f1e311a4ad4a243fb3bd6bae245e40f0
Malcolmston <67117350+Malcolmston@users.noreply.github.com>
2023-03-03 22:07:38 -0500

    remove

--- index.js
+++ index.js
@@ -96,123 +96,104 @@ function icon(type) {
 
 const {
   Guest_Account,
-Basic_Account,
-Admin_Account, File} = require("./sql.js");
+  Basic_Account,
+  Admin_Account,
+  File,
+} = require("./sql.js");
 
-
-
-var express = require('express');
+var express = require("express");
 var session = require("express-session");
-var formidable = require('formidable');
-var path = require('path');
-var fs = require('fs');
-var url = require("url")
+var formidable = require("formidable");
+var path = require("path");
+var fs = require("fs");
+var url = require("url");
 
-const file = new File()
-const guest_account = new Guest_Account()
-const basic_account = new Basic_Account()
-const admin_account = new Admin_Account()
+const file = new File();
+const guest_account = new Guest_Account();
+const basic_account = new Basic_Account();
+const admin_account = new Admin_Account();
 
-
-var ejs = require('ejs')
-
+var ejs = require("ejs");
 
 var app = express();
 
-
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 const sessionMiddleware = session({
-	secret: 'mysecret',
-	resave: true,
-	saveUninitialized: true
+  secret: "mysecret",
+  resave: true,
+  saveUninitialized: true,
 });
 
-
-
-
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(sessionMiddleware);
 
-app.get('/', (req, res) => {
-	res.status(200).render('login', {
-		code: 'sign up',
-		fail_code: req.session.username ? 'you are alredy logged in' : ''
-
-	});
+app.get("/", (req, res) => {
+  res.status(200).render("login", {
+    code: "sign up",
+    fail_code: req.session.username ? "you are alredy logged in" : "",
+  });
 });
 
-app.post('/signup', (req, res) => {
-	let { username, password } = req.body
+app.post("/signup", (req, res) => {
+  let { username, password } = req.body;
 
-	basic_account.validate(username, password).then(function(x){
-		// if there is an account with the username and password it is valid.
-		if( !x ){
-			basic_account.create(username, password).then(function(e){
-				req.session.username = username
-        req.session.type = 'basic'
+  basic_account.validate(username, password).then(function (x) {
+    // if there is an account with the username and password it is valid.
+    if (!x) {
+      basic_account.create(username, password).then(function (e) {
+        req.session.username = username;
+        req.session.type = "basic";
 
-				res.status(200).redirect('home')
-			})
-
-
-		}else{
-      res.redirect('/')
+        res.status(200).redirect("home");
+      });
+    } else {
+      res.redirect("/");
+      /*
 			res.status(403).render('login', {
 			code: 'an account with that username already exists!',
 			fail_code: 'an account with that username already exists!'
-		});
-		}
-	})
-})
-
-app.post('/login', (req, res) => {
-	let { username, password } = req.body
-  basic_account.isDeleted(username).then(function(bool){
-    if( bool ){
-      res.redirect('/')
-      res.status(403).render('login', {
-code: 'an account with that username or password no longer exists!',
-fail_code: 'The account you are trying to find an account that has been deleted!'
-});
-    }else{
-      basic_account.validate(username, password).then(function(x){
-        if( x ){
-          req.session.username = username
-
-          req.session.type = 'basic'
-
-          res.status(200).redirect('home')
-        }else{
-            admin_account.validate(username, password).then(function(x){
-            if( x ){
-                 req.session.username = username
-              req.session.type = 'admin'
-
-              res.status(200).redirect('home')
-            }else{
-              res.redirect('/')
-              res.status(402).render('login', {
-      code: 'an account with that username or password does not exist!',
-      fail_code: 'an account with that username or password does not exist!'
     });
-            }
-
-      })
-      }
-    })
+    */
     }
-  })
+  });
+});
 
-})
+app.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  basic_account.isDeleted(username).then(function (bool) {
+    if (bool) {
+    //  res.redirect("/");
+      res.status(403).render("login", {
+        code: "an account with that username or password no longer exists!",
+        fail_code:
+          "The account you are trying to find an account that has been deleted!",
+      });
+    } else {
+      basic_account.validate(username, password).then(function (x) {
+        if (x) {
+          req.session.username = username;
+          req.session.type = "basic";
+          res.redirect("/home");
+        } else {
+          //res.redirect("/");
+          res.status(402).render("login", {
+            code: "an account with that username or password does not exist!",
+            fail_code:
+              "an account with that username or password does not exist!",
+          });
 
+        }
+      });
+    }
+  });
+});
 
+/*
 app.post('/guest', (req, res) => {
   guest_account.create().then(function(x){
     req.session.username = 'guest'
@@ -228,126 +209,73 @@ app.post('/guest', (req, res) => {
 		});
   })
 })
+*/
 
-
-
-app.get('/home', (req, res) => {
-  if (!req.session.username) {
-console.log('Please enter a name')
-
-res.redirect('/')
-
-/*
-res.status(400).render('login', {
-  code: 'sign up',
-  fail_code: 'you tried to go to a page that you do not have permision to acses'
-
-});*/
-
-
-  }else{
-
-	if (req.session.username) {
-		res.render('home',{
-			type: req.session.type
-		})
-
-	} else {
-    res.redirect('/')
-
-		res.status(400).render('login', {
-			code: 'sign up',
-			fail_code: 'you tried to go to a page that you do not have permision to acses'
-
-		});
-  
-	}
-}
-})
-
-
-app.get('/logout', (req, res) => {
-  if( req.session.type == 'guest' ){
-    guest_account.removeNow().then(function(){
-      req.session.destroy()
-      res.redirect('/')
+app.get("/logout", (req, res) => {
+  if (req.session.type == "guest") {
+    guest_account.removeNow().then(function () {
+      req.session.destroy();
+      res.redirect("/");
       //return;
-    })
-  }else{
-	req.session.destroy()
-  res.redirect('/')
-}
-})
-
-
-
-
-
+    });
+  } else {
+    req.session.destroy();
+    res.redirect("/");
+  }
+});
 
 app.get("/home/get", async (req, res) => {
-
- let all = await file.findYours(req.session.username)
+  let all = await file.findYours(req.session.username);
 
   res.json({
     all: all,
-
   });
-
 });
 
 app.get("/home/open", async (req, res) => {
   const queryObject = url.parse(req.url, true).query;
 
-  let all = await file.find(queryObject.name)
+  let all = await file.find(queryObject.name);
 
-  res.setHeader('Content-Type', all.type)
-  res.end(all.rawData)
-
+  res.setHeader("Content-Type", all.type);
+  res.end(all.rawData);
 });
 
+/*
+app.post("/", function (req, res) {
+  var form = new formidable.IncomingForm();
 
-app.post('/', function (req, res){
-    var form = new formidable.IncomingForm();
+  form.parse(req);
 
-    form.parse(req);
+  form.on("fileBegin", function (name, file) {
+    file.path = __dirname + "/uploads/" + file.name;
+  });
 
-    form.on('fileBegin', function (name, file){
-        file.path = __dirname + '/uploads/' + file.name;
+  form.on("file", async function (name, files) {
+    var oldPath = files.filepath;
+    var newPath =
+      path.join(__dirname, "uploads") +
+      "/" +
+      (files.name || files.originalFilename);
+    var rawData = fs.readFileSync(oldPath);
+
+    let x = await file.create(req.session.username, {
+      name: files.name || files.originalFilename,
+      type: files.mimetype,
+      url: icon(files.mimetype.split("/")[1].trim()),
+      rawData: rawData,
+      flags: files.flags,
     });
 
-    form.on('file', async function (name, files){
-        
-        var oldPath = files.filepath;
-			var newPath = path.join(__dirname, 'uploads')
-				+ '/' + (files.name || files.originalFilename)
-			var rawData = fs.readFileSync(oldPath)
+    //res.setHeader('Content-Type',files.mimetype)
+    //res.end(rawData);
 
+   
+    //  console.log( file );
+  });
 
-              let x = await file.create(req.session.username,{ 
-                name:  (files.name || files.originalFilename), 
-                type: files.mimetype,
-                url: icon(files.mimetype.split('/')[1].trim()),
-                rawData: rawData,
-                flags: files.flags
-              });
-
-              
-              
-              //res.setHeader('Content-Type',files.mimetype)
-             //res.end(rawData);
-            
-
-              /*
-			fs.writeFile(newPath, rawData, function(err) {
-				if (err) console.log(err)
-			 console.log("Successfully uploaded")
-			})
-			*/
-      //  console.log( file );
-    });
-
-    res.status(204).send();
-   //res.redirect('/');
+  res.status(204).send();
+  //res.redirect('/');
 });
-
+*/
 app.listen(3000);
